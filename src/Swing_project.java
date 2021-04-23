96c66158ad5eee00072f9028e2e3d60bba1829ad
Kazuke <to.jest_test@onet.pl>
2021-04-23 15:02:42 +0200

    Second changes

---
+++ src/Swing_project.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Swing_project {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Swing_project::CreateNewGUI);
+    }
+
+    private static void CreateNewGUI() {
+        JFrame frame = new JFrame();
+    }
+}
