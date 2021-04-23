8b8171a2240e1ec63c05090b18f04497b297e71f
Kazuke <to.jest_test@onet.pl>
2021-04-23 15:00:58 +0200

    Second changes

--- src/swing_project.java
+++ src/swing_project.java
@@ -2,12 +2,7 @@ import javax.swing.*;
 
 public class swing_project {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                CreateNewGUI();
-            }
-        });
+        SwingUtilities.invokeLater(swing_project::CreateNewGUI);
     }
 
     private static void CreateNewGUI() {
