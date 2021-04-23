7b41af06dd49e8080be01e5aaed757e8c41e2eaa
Kazuke <to.jest_test@onet.pl>
2021-04-23 15:00:13 +0200

    First changes

--- src/swing_project.java
+++ src/swing_project.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class swing_project {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                CreateNewGUI();
+            }
+        });
+    }
 
+    private static void CreateNewGUI() {
     }
 }
